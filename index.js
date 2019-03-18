5b3a060196bd5f1a4d5d382efe54a2f16ab657c8
sean-attewell <sean.attewell@gmail.com>
2019-03-18 15:55:08 +0000

    get and post requests workign

--- index.js
+++ index.js
@@ -8,14 +8,56 @@ const db = require('./data/db');
 
 // creates an express application using the express module
 const server = express();
+server.use(express.json());
 
-
-server.get('/users', (req, res) => {
+server.get('/api/users', (req, res) => {
     db.find()
         .then(data => {
             res.json(data)
         })
+        .catch(err => {
+            res.status(500).json({ "error": "The users information could not be retrieved." });
+        }
+        )
 })
+
+server.post('/api/users', (req, res) => {
+    const { name, bio } = req.body;
+    if (!name || !bio) {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." });
+        return;
+    }
+    db.insert({
+        name,
+        bio
+    })
+        .then(response => {
+            res.status(201).json(response);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ errorMessage: "There was an error while saving the user to the database." });
+            return;
+        });
+});
+
+
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    db.findById(id)
+        .then(user => {
+            if (user.length === 0) {
+                res.status(404).json({ errorMessage: "User with that id not found." });
+                return;
+            }
+            res.json(user);
+        })
+        .catch(error => {
+            res.status(500).json({ errorMessage: "Error looking up user" });
+        });
+    // invoke proper db.method(id) passing it the id.
+    // handle the promise like above
+});
 
 
 // configures our server to execute a function for every GET request to "/"
